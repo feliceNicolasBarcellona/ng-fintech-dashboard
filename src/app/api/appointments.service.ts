6793dfaf37709f96982ea7d2bb73c4cb46ee62c8
Felice Nicolas Barcellona <felicebarcellona1@gmail.com>
2023-11-04 21:43:33 +0100

    add appointments service

---
+++ src/app/api/appointments.service.ts
@@ -0,0 +1,26 @@
+import { DayWithSlot } from './../models/day-with-slot';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { DayWithSlots } from '../models/day-with-slots';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppointmentsService {
+
+  constructor(private http: HttpClient) { }
+
+  getLocations(): Observable<Location[]>{
+    return this.http.get<Location[]>(environment.apiUrl + '/locations')
+  }
+
+  getSlotsById(locationId: string): Observable<DayWithSlots[]>{
+    return this.http.get<DayWithSlots[]>(environment.apiUrl + '/slots/' + locationId)
+  }
+
+  schedule(dayWithSlot: DayWithSlot): Observable<boolean>{
+    return this.http.post<boolean>(environment.apiUrl + '/schedule', dayWithSlot)
+  }
+}
